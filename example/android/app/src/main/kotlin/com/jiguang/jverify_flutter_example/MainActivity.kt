4dfc826563a9248ceba6aa17add1bef7debb6d4a
gohopo <gohopo@live.com>
2025-04-21 13:52:36 +0800

    feat: hm

--- example/android/app/src/main/kotlin/com/jiguang/jverify_flutter_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.jiguang.jverify_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/jverify_flutter.dart
+++
@@ -1,8 +0,0 @@
-
-import 'jverify_flutter_platform_interface.dart';
-
-class JverifyFlutter {
-  Future<String?> getPlatformVersion() {
-    return JverifyFlutterPlatform.instance.getPlatformVersion();
-  }
-}
